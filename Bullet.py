fe02ee94b949dbd524bf9cad7ab4cb387dabca75
maiklfarb <MishaFarb09@gmail.com>
2022-06-20 17:22:16 +0300

    Add Movement

---
+++ Bullet.py
@@ -0,0 +1,32 @@
+from pygame.sprite import Sprite
+import pygame
+
+
+class Bullet(Sprite):
+    def __init__(self, screen, settings, ship, _type="default"):
+        super().__init__()
+        self._type = _type
+        self.settings = settings
+        self.screen = screen
+        self.ship = ship
+        self.colour = self.settings.bullet_colour
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_wight, self.settings.bullet_height)  # x, y, ширина, высота
+        self.rect.midbottom = self.ship.rect.midtop
+
+        self.y = float(self.rect.y)
+        self.x = float(self.rect.x)
+
+    def update(self):
+        self.y -= self.settings.bullet_speed
+        self.rect.y = self.y
+
+        if self._type == "right":
+            self.x += self.settings.bullet_speed
+            self.rect.x = self.x
+        elif self._type == "left":
+            self.x -= self.settings.bullet_speed
+            self.rect.x = self.x
+
+
+    def blitme(self):
+        pygame.draw.rect(self.screen, self.colour, self.rect)  # где рисуем, цвет, что рисуем
